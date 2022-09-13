f903001cf83ed38e02fa77744556e0a1e36fac14
Certified Humane <castrolwilliam@gmail.com>
2022-09-13 11:47:15 -0400

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,1 +1,14 @@
-import
+from xml.etree.ElementInclude import include
+from setuptools import setup, find_packages
+
+setup(
+    name='bts7960',
+    version='0.0.1',
+    description="Gpio program to controll the BTS7960 of a robotics project",
+    author="Louis Castro",
+    author_email="castrolwilliam@gmail.com",
+    packages=find_packages(include=['lib','lib.*']),
+    install_requires=[
+        'RPi.GPIO'
+    ]
+);
