1f2b068bf16905e8abc2a324624fd0cc770c6efb
Certified Humane <castrolwilliam@gmail.com>
2022-09-14 09:44:08 -0400

    Add files via upload

---
+++ python/example.py
@@ -0,0 +1,6 @@
+import bts7960
+import time
+MOV = md.Drivers();
+
+
+MOV.Test_All();
--- python/setup.py
+++ python/setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
 
+setup(
+    name='bts7960',
+    version='0.1',
+    description='this a gpio project for the motor driver 7960 for the use of robot autonomy',
+    url='#',
+    author='Louis W. Castro',
+    author_email='castrolwilliam@gmail.com',
+    license='MIT',
+    packages=['bts7960'],
+    zip_safe=False
+)
