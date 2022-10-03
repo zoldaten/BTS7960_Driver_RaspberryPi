e78be41b30bb4ba7ad580be72ae89b5a0ebb4751
Certified Humane <castrolwilliam@gmail.com>
2022-10-03 07:09:56 -0400

    Update __init__.py

--- python/bts7960/__init__.py
+++ python/bts7960/__init__.py
@@ -71,27 +71,27 @@ class Drivers():
 
 
     def Forward(self):
-        self.rpwm_D1.ChangeDutyCycle(40)
-        self.rpwm_D2.ChangeDutyCycle(40)
+        self.rpwm_D1.ChangeDutyCycle(50)
+        self.rpwm_D2.ChangeDutyCycle(50)
         time.sleep(.125);
         self.rpwm_D1.ChangeDutyCycle(0)
         self.rpwm_D2.ChangeDutyCycle(0)
     
     def Rotate_Right(self):
-        self.rpwm_D2.ChangeDutyCycle(40)
+        self.rpwm_D2.ChangeDutyCycle(50)
         time.sleep(.125)
         self.rpwm_D2.ChangeDutyCycle(0)
         
     
     def Rotate_Left(self):
-        self.rpwm_D1.ChangeDutyCycle(40)
+        self.rpwm_D1.ChangeDutyCycle(50)
         time.sleep(.125)
         self.rpwm_D1.ChangeDutyCycle(0)
         
         
     def Backward(self):
-        self.lpwm_D1.ChangeDutyCycle(40)
-        self.lpwm_D2.ChangeDutyCycle(40)
+        self.lpwm_D1.ChangeDutyCycle(50)
+        self.lpwm_D2.ChangeDutyCycle(50)
         time.sleep(.125);
         self.lpwm_D1.ChangeDutyCycle(0)
         self.lpwm_D2.ChangeDutyCycle(0)
